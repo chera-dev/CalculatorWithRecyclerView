4843af9e3e4cfdfa8144cf6988d5920cecc45431
chera-dev <cheraganesh832000@gmail.com>
2021-06-28 19:16:38 +0530

    passing data back to main activity

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonSub: Button
     private lateinit var buttonMultiply: Button
     private lateinit var buttonDivide: Button
+    private lateinit var textView: TextView
+    private lateinit var buttonReset:Button
+    private lateinit var action: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,21 +26,18 @@ class MainActivity : AppCompatActivity() {
         buttonSub = binding.buttonSub
         buttonMultiply = binding.buttonMultiply
         buttonDivide = binding.buttonDivide
-        val intent = getIntent()
-        val num1 = intent.getStringExtra("num1")
-        val num2 = intent.getStringExtra("num2")
-        val result = intent.getDoubleExtra("result",0.00)
-        val action = intent.getStringExtra("action")
-        if(num1!=null){
-            showResult(num1,num2,result,action)
-        }
+        textView= binding.textViewresult
+        buttonReset= binding.buttonReset
+        buttonReset.setOnClickListener { onReset() }
     }
 
     fun onButtonClick(view: View){
-        val action: String = getButtonText(view)
+        action= getButtonText(view)
         val intent= Intent(this,MainActivity2::class.java)
         intent.putExtra("Action",action)
-        startActivity(intent)
+        //startActivity(intent)
+        //get data from second activity
+        startActivityForResult(intent,1)
     }
 
     private fun getButtonText(view: View):String{
@@ -47,27 +48,45 @@ class MainActivity : AppCompatActivity() {
             else -> buttonDivide.text.toString()
         }
     }
-
-    private fun showResult(num1: String, num2: String?, result: Double, action: String?) {
-        val num1TextView:TextView = findViewById(R.id.textViewresult)
-        val buttonReset:Button = findViewById(R.id.buttonReset)
-        num1TextView.visibility = View.VISIBLE
-        buttonReset.visibility = View.VISIBLE
-        buttonAdd.visibility=View.INVISIBLE
-        buttonSub.visibility=View.INVISIBLE
-        buttonMultiply.visibility=View.INVISIBLE
-        buttonDivide.visibility=View.INVISIBLE
-        num1TextView.text = "Action :  $action\nInput1 :  $num1\nInput2 :  $num2\nResult :  $result"
-
-        buttonReset.setOnClickListener {
-            finish()
-            //destroying the activity instead of making views visible and invisible
-            /*num1TextView.visibility = View.INVISIBLE
-            buttonReset.visibility = View.INVISIBLE
-            buttonAdd.visibility=View.VISIBLE
-            buttonSub.visibility=View.VISIBLE
-            buttonMultiply.visibility=View.VISIBLE
-            buttonDivide.visibility=View.VISIBLE*/
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==1){
+            if(resultCode == RESULT_OK){
+                textView.visibility = View.VISIBLE
+                buttonReset.visibility = View.VISIBLE
+                buttonAdd.visibility=View.INVISIBLE
+                buttonSub.visibility=View.INVISIBLE
+                buttonMultiply.visibility=View.INVISIBLE
+                buttonDivide.visibility=View.INVISIBLE
+                if (data != null) {
+                    val num1 = data.getStringExtra("num1")
+                    val num2 = data.getStringExtra("num2")
+                    val actionResult = operation(num1?.toDoubleOrNull(), num2?.toDoubleOrNull(),action)
+                    textView.text = "Action :  $action\nInput1 :  $num1\nInput2 :  $num2\nResult :  $actionResult"
+                }
+            }
+        }
+    }
+    fun onReset(){
+        textView.visibility = View.INVISIBLE
+        buttonReset.visibility = View.INVISIBLE
+        buttonAdd.visibility=View.VISIBLE
+        buttonSub.visibility=View.VISIBLE
+        buttonMultiply.visibility=View.VISIBLE
+        buttonDivide.visibility=View.VISIBLE
+    }
+    private fun operation(num1: Double?, num2: Double?, action: String?):Double?{
+        if(num1!=null && num2!=null && action!=null){
+            return when(action){
+                "ADDITION" -> num1+num2
+                "SUB" -> num1-num2
+                "MULTIPLY" -> num1*num2
+                else -> num1/num2
+            }
+        }
+        else{
+            Toast.makeText(this, "enter valid number and try again", Toast.LENGTH_LONG).show()
+            return null
         }
     }
 }
--- app/src/main/java/com/example/calculator/MainActivity2.kt
+++ app/src/main/java/com/example/calculator/MainActivity2.kt
@@ -17,7 +17,6 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-        var actionResult :Double?
         val intent = getIntent()
         val action = intent.getStringExtra("Action")
         val button: Button = binding.buttonResult
@@ -25,32 +24,14 @@ class MainActivity2 : AppCompatActivity() {
         num2 =binding.editTextNumber2
         button.text  = action
         button.setOnClickListener {
-            //finish()
-            actionResult = operation(num1.text.toString().toDoubleOrNull(),num2.text.toString().toDoubleOrNull(),action)
-            actionResult?.let { toMainActivity(action, it) }
+            toMainActivity(action)
         }
     }
-    private fun toMainActivity(action: String?, actionResult: Double){
+    private fun toMainActivity(action: String?){
         val intent = Intent(this,MainActivity::class.java)
         intent.putExtra("num1",num1.text.toString())
         intent.putExtra("num2",num2.text.toString())
-        intent.putExtra("result",actionResult)
-        intent.putExtra("action",action )
-        startActivity(intent)
+        setResult(RESULT_OK,intent)
         finish()
-    }
-    private fun operation(num1: Double?, num2: Double?, action: String?):Double?{
-        if(num1!=null && num2!=null && action!=null){
-            return when(action){
-                "ADDITION" -> num1+num2
-                "SUB" -> num1-num2
-                "MULTIPLY" -> num1*num2
-                else -> num1/num2
-            }
-        }
-        else{
-            Toast.makeText(this, "enter number and try again", Toast.LENGTH_SHORT).show()
-            return null
-        }
     }
 }
